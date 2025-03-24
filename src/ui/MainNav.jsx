7bb27e7c4973590964671abedab31eab8b5fac78
Sahil Suthar <sahil26dd@gmail.com>
2025-03-24 23:29:56 +0530

    lect 335: Building the sidebar and the main navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import {
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+  HiOutlineCalendar,
+  HiOutlineCog6Tooth,
+} from "react-icons/hi2";
+import { NavLink } from "react-router-dom";
 
 const NavList = styled.ul`
   display: flex;
@@ -6,7 +14,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +52,59 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+const NAV_LINKS = [
+  {
+    id: 1,
+    name: "Home",
+    route: "/dashboard",
+    icon: HiOutlineHome,
+  },
+  {
+    id: 2,
+    name: "Bookings",
+    route: "/bookings",
+    icon: HiOutlineCalendar,
+  },
+  {
+    id: 3,
+    name: "Cabins",
+    route: "/cabins",
+    icon: HiOutlineHomeModern,
+  },
+  {
+    id: 4,
+    name: "Users",
+    route: "/users",
+    icon: HiOutlineUsers,
+  },
+  {
+    id: 5,
+    name: "Settings",
+    route: "/settings",
+    icon: HiOutlineCog6Tooth,
+  },
+];
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        {NAV_LINKS.map((cur) => {
+          const IconComponent = cur?.icon;
+
+          return (
+            <li key={cur?.id}>
+              <StyledNavLink to={cur?.route}>
+                <IconComponent />
+                <span>{cur?.name}</span>
+              </StyledNavLink>
+            </li>
+          );
+        })}
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
