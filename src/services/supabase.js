10bbf95908aa1e6bda692d357e7950a782ccfd1f
Sahil Suthar <sahil26dd@gmail.com>
2025-03-31 17:22:41 +0530

    lect 347: setting up react query

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://oiacuuealxphliibmqof.supabase.co";
-// const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pYWN1dWVhbHhwaGxpaWJtcW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxODE5NzcsImV4cCI6MjA1ODc1Nzk3N30.0OK_zFc47SFWhnaA0EwEMyNmbOHoMfGBQxRAl0WpgTU";
 
