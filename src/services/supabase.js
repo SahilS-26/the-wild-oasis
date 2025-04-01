a81bb04df968a03cff3c29c86199cac256def1ba
Sahil Suthar <sahil26dd@gmail.com>
2025-04-01 23:36:57 +0530

    lect 355: uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oiacuuealxphliibmqof.supabase.co";
+export const supabaseUrl = "https://oiacuuealxphliibmqof.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pYWN1dWVhbHhwaGxpaWJtcW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxODE5NzcsImV4cCI6MjA1ODc1Nzk3N30.0OK_zFc47SFWhnaA0EwEMyNmbOHoMfGBQxRAl0WpgTU";
