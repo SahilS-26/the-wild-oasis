ecc142ca673a8b009b073158a62b8ac60f5bdb93
Sahil Suthar <sahil26dd@gmail.com>
2025-05-08 22:24:59 +0530

    fix: initial load query error on bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -14,7 +14,7 @@ export function useBookings() {
   // { field: "totalPrice", value: 5000, method: "gte" };
 
   // SORT
-  const sortByRaw = searchParams.get("sortBy");
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
