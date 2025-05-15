637c32fcfa1d569593ceffce34c44f662dde7926
Sahil Suthar <sahil26dd@gmail.com>
2025-05-15 23:19:01 +0530

    lect 385: checking in a booking

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -117,6 +117,7 @@ export async function updateBooking(id, obj) {
     console.error(error);
     throw new Error("Booking could not be updated");
   }
+  console.log("Hook triggered");
   return data;
 }
 
